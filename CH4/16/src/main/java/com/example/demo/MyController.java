7422f142e0a0cb84bbd6b6ec1b991c8075c427fa
22447887 <49071263+22447887@users.noreply.github.com>
2022-07-15 20:32:59 +0800

    16
    攔截器Interceptor

---
+++ CH4/16/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/test1")
+    public String test1(){
+        System.out.println("執行 test1 方法");
+        return "Hello test1";
+    }
+
+    @RequestMapping("/test2")
+    public String test2(){
+        System.out.println("執行 test2 方法");
+        return "Hello test2";
+    }
+}
---
+++ CH4/16/src/main/java/com/example/demo/MyInterceptor.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("執行 MyInterceptor 的 preHandle 方法");
+        response.setStatus(401);
+        return false;
+    }
+}
