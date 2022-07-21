6d9fdc63e7cf7353f3bf58e14413f5e89271d62a
22447887 <49071263+22447887@users.noreply.github.com>
2022-07-21 21:37:14 +0800

    5-7
    總結query

---
+++ CH5/7/src/main/java/com/example/demo/StudentRowMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentRowMapper implements RowMapper<Student> {
+
+    @Override
+    public Student mapRow(ResultSet resultSet, int i) throws SQLException {
+        Student student = new Student();
+        student.setId(resultSet.getInt("id"));
+        student.setName(resultSet.getString("name"));
+
+        return student;
+    }
+}
